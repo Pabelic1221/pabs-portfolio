5eb3669871417341eb84cfba86068f7b31ef760d
Pabelic1221 <pabelicjush@gmail.com>
2025-05-04 15:54:19 +0800

    Update Hero component to reflect correct name

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     <section className="min-h-screen text-white flex items-center justify-center px-10">
       <div className="max-w-3xl">
         <p className="text-teal-400 text-base">Hi, my name is</p>
-        <h1 className="text-5xl font-bold text-white mt-2">Brittany Chiang.</h1>
+        <h1 className="text-5xl font-bold text-white mt-2">Jushua Pabelic.</h1>
         <h2 className="text-5xl font-bold text-gray-400 mt-2">I build things for the web.</h2>
         <p className="text-gray-400 text-base mt-6">
           I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.
